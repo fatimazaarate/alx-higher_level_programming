c5b1b0bdef6e0c7edc766f45da7ebc73ab3e1f1e
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-03 19:13:29 +0100

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print(i, end = '')
-    elif i < 10:
-        print("0{}, " .format(i), end = '')
-    elif i > 9:
-        print(i, ", ", end = '')
+        print("{} " .format(i), end = '')
+    else:
+        print("{:02}, " .format(i), end = '')
+
     
