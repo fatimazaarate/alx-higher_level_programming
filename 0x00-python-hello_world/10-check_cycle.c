1e67b376aff4d8e7812d95dba18c0c335b39dca7
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-04 21:13:24 +0100

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+    listint_t* ptr1 = list;
+    listint_t* ptr2 = list;
+
+    while (ptr1 && ptr2 && ptr2->next)
+    {
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next->next;
+
+        if (ptr1 == ptr2)
+            return (1);
+    }
+    return (0);
+}
