2bd19dc4808eacee636d0903c723ba80d581f277
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-05 16:11:24 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,14 @@
 if __name__ == "__main__":
     import sys
 
-arg_number = len(sys.argv)
-i = 1
+arg_number = len(sys.argv) - 1
 
 if arg_number == 0:
-    print("{} arguments." .format(arg_number - 1))
+    print("{} arguments." .format(arg_number))
 elif arg_number == 1:
-    print("{} argument." .format(arg_number - 1))
-    print("{}: {}" .format(arg_number, sys.argv[1]))
+    print("{} argument." .format(arg_number))
 else:
-    print("{} arguments:" .format(arg_number - 1))
-    while i < arg_number:
-        print("{}: {}" .format(i, sys.argv[i]))
-        i = i+1
+    print("{} arguments:" .format(arg_number))
+for i in range(arg_number):
+    print("{}: {}" .format(i+1, sys.argv[i+1]))
+
