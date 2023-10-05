c425519d0f04c7e4bd927d7bfa9363ece89cc7f9
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-05 10:59:47 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 if __name__ == "__main__":
-    from calculator_1 import add, div, sub, mul
+    from calculator_1 import add, sub, div, mul
 
 a = 10
 b = 5
@@ -8,11 +8,11 @@ b = 5
 sum = add(a, b)
 print("{} + {} = {}" .format(a, b, sum))
 
-division = div(a, b)
-print("{} + {} = {}" .format(a, b, division))
-
 subs = sub(a, b)
 print("{} + {} = {}" .format(a, b, subs))
 
 multi = mul(a, b)
 print("{} + {} = {}" .format(a, b, multi))
+
+division = div(a, b)
+print("{} + {} = {}" .format(a, b, division))
