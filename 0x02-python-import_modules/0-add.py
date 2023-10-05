bbcdd74f751cb58cd60028c16058d7f35c2d54af
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-05 10:08:18 +0100

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add 
 
-sum = add(1, 2)
-print("{} + {} = {}" .format(1, 2, sum))
+a = 1
+b = 2
+sum = add(a, b)
+print("{} + {} = {}" .format(a, b, sum))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import calculator_1 as c
+
+sum = c.add(10, 5)
+print("{} + {} = {}" .format(10, 5, sum))
+
+division = c.div(10, 5)
+print("{} + {} = {}" .format(10, 5, division))
+
+subs = c.sub(10, 5)
+print("{} + {} = {}" .format(10, 5, subs))
+
+multi = c.mul(10, 5)
+print("{} + {} = {}" .format(10, 5, multi))
