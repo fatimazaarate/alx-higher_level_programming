cee4a99a120ebf7c34341dd9ac4e1141ea274485
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-07 13:05:16 +0100

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copied_list = my_list.copy()
 
     if 0 <= idx < len(my_list):
-        my_list[idx] = element
-        return my_list
-    else:
+        copied_list[idx] = element
         return copied_list
+    else:
+        return my_list
