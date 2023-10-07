6fe4ce14f262ecb02852affa01ac50e2fac46ecb
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-07 19:35:59 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    result = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            result += char
-    return result
+    new_string = [char for char in my_string if char != 'c' and char != 'C']
+    return ''.join(new_string)
