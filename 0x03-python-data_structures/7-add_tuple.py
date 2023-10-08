a2737a970add44c7eace561241e7dc56edeb386e
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-08 12:53:26 +0100

    tupple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = ()
-    max_length = max(len(tuple_a), len(tuple_b))
-    tuple_a = tuple_a + (0,) * (max_length - len(tuple_a))
-    tuple_b = tuple_b + (0,) * (max_length - len(tuple_b))
-    
-    for i in range(len(tuple_a)):
-        new_tuple += (tuple_a[i] + tuple_b[i],)
-    return new_tuple
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
