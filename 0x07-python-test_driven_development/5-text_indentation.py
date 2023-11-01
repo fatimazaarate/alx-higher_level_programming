53b2c347fbdb2cd80699a6aea8f1b7ca332a4137
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-01 21:40:37 +0100

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 '''
-Module for text_indentation method
-it prints a text with 2 new lines after each of these characters: ., ? and :
-returns nothing
+function that prints a text with 2 new lines
+after these chars: .,? and:
+text must be a str else raise TypeError
+There should be no space at the beginning
+or at the end of each printed line
 '''
 
 
 def text_indentation(text):
     '''
-    Function prints a that text with 2 new lines.
+    this function prints a text with 2 new lines.
     '''
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in range(len(text)):
-        if text[i] in [".", "?", ":"]:
-            print("{}\n".format(text[i]))
-        elif text[i] == " " and text[i - 1] in [".", "?", ":"]:
+    for a in range(len(text)):
+        if text[a] in [".", "?", ":"]:
+            print("{}\n".format(text[a]))
+        elif text[a] == " " and text[a - 1] in [".", "?", ":"]:
             continue
         else:
-            print("{}".format(text[i]), end="")
+            print("{}".format(text[a]), end="")
