9b2d7badcb724009a23930f4b257d678908b8c90
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-06 22:02:30 +0100

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-a class BaseGeometry
+Defines Rectangle module, an inheritor of BaseGeometry
 """
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-    Public instance method: def area(self):
-    that raises an Exception with the message area()
-    is not implemented
+    represent rectangle using BaseGeometry
     """
 
     def __init__(self, width, height):
-        '''
-        width and height must be positive integers
-        '''
+        """
+        __init__ method initializes the instances attributes.
 
-        self.__width = width
+        Args:
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
+        """
+
         self.integer_validator("width", width)
-        self.__height = height
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
