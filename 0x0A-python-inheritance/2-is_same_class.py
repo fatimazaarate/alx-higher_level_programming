4a831ce5a7cb8a986936cade4b60481e4df0044d
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-06 17:43:04 +0100

    task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -18,7 +18,7 @@ def is_same_class(obj, a_class):
     its subclasses, False otherwise.
     """
 
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
-    else:
-        return False
+    
+    return False
