745916d875ac8e82119fe77fc515d297aaa6ff58
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-06 18:52:27 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -19,6 +19,6 @@ def inherits_from(obj, a_class):
     but not the exact same class as 'a_class'. False otherwise.
     """
 
-    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
