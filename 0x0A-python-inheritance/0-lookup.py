f71b721aedbb955f49fce8c970fe2075a169a271
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-06 14:48:10 +0100

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 
 def lookup(obj):
     '''
-    A function that returns the list of available attributes and methods of an object
+    A function that returns the list of available
+    attributes and methods of an object
     '''
     return dir(obj)
