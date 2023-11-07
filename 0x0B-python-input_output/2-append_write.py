c99d2830a4e3a0c43e1c61d217ccb12eec0eb7bc
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-07 16:25:47 +0100

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ a function that writes a string to a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Writes the specified text to a file with the given filename.
+
+    Args:
+        filename (str): The name of the file to write the text to.
+        text (str): The text to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
