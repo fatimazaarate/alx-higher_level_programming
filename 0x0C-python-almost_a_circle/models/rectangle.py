7b4621bee0a6a944e93aef3cc42a390606c5c9c4
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-13 19:27:52 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,12 @@ Define Rectangle module
 """
 from models.base import Base
 
+
 class Rectangle(Base):
     """
     creates a class that represents a Rectangle
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
@@ -40,27 +42,30 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-
     @property
     def width(self):
+        """the width attribute getter"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
+        """the width attribute setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
         if value <= 0:
             raise ValueError("width must be > 0")
-        
+
         self.__width = value
 
     @property
     def height(self):
+        """the height attribute getter"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
+        """the height attribute setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
@@ -68,13 +73,15 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
 
         self.__height = value
-        
+
     @property
     def x(self):
+        """the x attribute getter"""
         return self.__x
-        
+
     @x.setter
     def x(self, value):
+        """the x attribute setter"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
 
@@ -85,10 +92,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """the y attribute getter"""
         return self.__y
-        
+
     @y.setter
     def y(self, value):
+        """the y attribute setter"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
 
@@ -98,27 +107,32 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """a public method  that returns the area value of
+        the Rectangle instance"""
         return self.width * self.height
 
-    # def display(self):
-    #     for row in range(self.height):
-    #         for col in range(self.width):
-    #             print ("#", end='')
-    #         print()
     def display(self):
+        """a public method that prints in stdout
+        the Rectangle instance with the character #
+        by taking care of x and y"""
         rec = ['#' * self.width for _ in range(self.height)]
 
         for y in range(self.y):
             print(" " * self.y)
 
         for item in rec:
-                print(" " * self.x, end='')
-                print(item)
+            print(" " * self.x, end='')
+            print(item)
 
     def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        """the __str__ method so that it returns
+        [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return f"[Rectangle]({self.id}) {self.x}/{self.y} \
+        - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
+        """a public method that assigns an argument to each attribute
+        or assigns a key/value argument to attributes"""
         if args:
             attr = ["id", "width", "height", "x", "y"]
             for idx, value in enumerate(args):
@@ -129,10 +143,12 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """a public method  that returns
+        the dictionary representation of a Rectangle"""
         return {
-            "x" : self.x,
-            "y" : self.y,
-            "id" : self.id,
-            "height" : self.height,
-            "width" : self.width,
+            "x": self.x,
+            "y": self.y,
+            "id": self.id,
+            "height": self.height,
+            "width": self.width,
         }
