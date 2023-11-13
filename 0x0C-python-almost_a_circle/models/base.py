c24a1e97290c79b97207f7b0dd368976cfca16be
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-13 19:57:56 +0100

    square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """a static method that returns the JSON string representation of
         list_dictionaries"""
