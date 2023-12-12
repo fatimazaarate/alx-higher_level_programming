b61db7b3f785ddaae652bc00f30c78828c2f8f97
fatimazaarate <fatima.zaarate@gmail.com>
2023-12-12 20:55:58 +0100

    create second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,11 @@
 -- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-insert into second_table (1, "John", 10);
-insert into second_table (2, "Alex", 3);
-insert into second_table (3, "Bob", 14);
-insert into second_table (4, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table values (1, "John", 10);
+INSERT INTO second_table values (2, "Alex", 3);
+INSERT INTO second_table values (3, "Bob", 14);
+INSERT INTO second_table values (4, "George", 8);
