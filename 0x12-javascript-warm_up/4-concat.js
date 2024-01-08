deb479bc1bdabbfdd67141349df464841391539c
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-08 18:00:34 +0100

    task

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
 
-const args1 = process.argv[2];
-const args2 = process.argv[3];
-
-if (args1 === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(process.argv[2] + ' is', process.argv[3]);
