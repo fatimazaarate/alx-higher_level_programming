cae6218764507f21b5032d3fcf0cb8fcec08cde2
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-09 17:43:06 +0100

    modify

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -12,7 +12,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let str = '' /* empty str */
+      let str = ''; /* empty str */
       for (let j = 0; j < this.width; j++) {
         str += 'X';
       }
