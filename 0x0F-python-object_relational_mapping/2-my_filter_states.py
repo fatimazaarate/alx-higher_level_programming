21902edeb23707e9eec9087c657711810f91ede3
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-12 10:22:41 +0100

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,8 @@ import sys
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
     mydb = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -17,12 +19,11 @@ if __name__ == "__main__":
         password=sys.argv[2],
         database=sys.argv[3]
     )
-    state_name = sys.argv[4]
-
+    
     mycursor = mydb.cursor()
     mySQLformulat = "SELECT * \
         FROM states WHERE name = '{:s}' \
-        ORDER BY id ASC".format(state_name)
+        ORDER BY id ASC".format(sys.argv[4])
 
     mycursor.execute(mySQLformulat)
     states = mycursor.fetchall()
