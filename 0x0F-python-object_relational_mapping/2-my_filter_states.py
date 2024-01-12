d52b95bc2b42124c6025d1c03c457c8871451074
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-12 10:15:18 +0100

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """
-    Connects to a MySQL database and retrieves and prints data
-    from the 'states' table.
-
-    Usage: python script.py <user> <password> <database>
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    mydb = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
-    )
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3]
+                        )
+    mycursor = db.cursor()
+    sql_query = """SELECT * \
+            FROM states WHERE name = '{:s}' \
+                    ORDER by id ASC""".format(sys.argv[4])
+    mycursor.execute(sql_query)
+    rows = mycursor.fetchall()
 
-    mycursor = mydb.cursor()
-    mySQLformulat = "SELECT * \
-        FROM states WHERE name = '{:s}' \
-        ORDER BY id ASC".format(sys.argv[4])
-
-    mycursor.execute(mySQLformulat)
-    states = mycursor.fetchall()
-
-    for state in states:
-        print(state)
+    for row in rows:
+        print(row)
