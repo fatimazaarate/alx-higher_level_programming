a58fe91670e2da3664ccde3e9b359a0d671444c0
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-12 17:29:54 +0100

    task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class State(Base):
     """create class State for MySQL database
 
@@ -20,5 +21,5 @@ class State(Base):
 
     __tablename__ = "states"
 
-    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
