2e938e7e9d95b5aefb3885b62af9063543a1093c
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-25 22:19:52 +0100

    task

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sX POST -d @"$2" "$1"
+response=$(curl -sX POST -d @"$2" "$1")
+echo "$response"
