1aeb2736417658a14f4654479f5cd45db601bfb7
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-26 18:30:54 +0100

    task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL
+sends a request to the URL and displays the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(f"Your email is: {page}")
