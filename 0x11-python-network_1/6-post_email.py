5c44b71ffb7e538d90dd5d5d15882f1bd2f296cb
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-26 20:48:22 +0100

    task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL
+sends a request to the URL and displays the body of the response
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    req = requests.post(url, value)
+    print(f"{req}")
