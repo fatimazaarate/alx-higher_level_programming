dd5cc1859c3c1fba2856b7b7ceba103021241180
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-26 18:38:19 +0100

    task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     value = {'email': sys.argv[2]}
 
     data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
+    data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
