2d18abd0dd43a38de0041be427e1c44cfa8155fe
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-26 19:50:59 +0100

    task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as response:
             page = response.read()
     except urllib.error.HTTPError as e: 
-        print("Error code: ", e.code)
+        print("Error code:", e.code)
     else:
         print(f"{page.decode('utf-8')}")
