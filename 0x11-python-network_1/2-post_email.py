dea803e312071d6c6d6bc1ddb43548c99d0f3f74
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-26 18:41:58 +0100

    task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode(value)
+    data = urllib.parse.urlencode(value).encode("utf-8")
     data = data.encode('utf-8')
 
     req = urllib.request.Request(url, data=data, method='POST')
