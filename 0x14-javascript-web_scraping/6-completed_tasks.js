366c8b1c6acf8431b22a8516ade1e9b30fb61261
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-20 22:34:23 +0100

    task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, { json: true }, (error, response, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const task = {};
+
+    for (const user of data) {
+        if (user.completed) {
+            if (task[user.userId]) {
+                task[user.userId]++;
+        } else {
+            task[user.userId] = 1;
+        }
+    }
+    }
+    console.log(task);
+});
