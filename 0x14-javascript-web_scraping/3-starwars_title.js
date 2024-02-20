0b57dad8fada9943a6e3a8ce888d74c066afceea
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-20 19:26:29 +0100

    task

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const parse = JSON.parse(body);
+  const filmTitle = parse.title; 
+  console.log(filmTitle);
+});
