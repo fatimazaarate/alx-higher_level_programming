d17c8d9784b4ce2f1753053d5d1f5205397b612f
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-20 19:06:26 +0100

    task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const url = process.argv[2];
-
-request(url, (error, response) => {
+const Arguments = process.argv;
+request(Arguments[2], (error, response) => {
   if (error) {
     console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
   }
-  console.log('code: ', response.statusCode);
 });
